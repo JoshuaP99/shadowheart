b2dceb68314ed6864e800fc3c85cc39a8dcae2f3
JoshuaP99 <pikminguy56@gmail.com>
2023-08-08 15:45:14 -0600

    bot initialization

---
+++ commands/playthrough.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+const races = ["Elf", "Tiefling", "Drow", "Human", "Githyanki", "Dwarf", "Half-Elf", "Halfling", "Gnome", "Dragonborn", "Half-Orc"]
+const classes = ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"]
+const alignment = ["Neutrality", "Heroism", "Evil"]
+
+const randomRace = races[Math.floor(Math.random() * 11)];
+const randomClass = classes[Math.floor(Math.random() * 12)];
+const randomAlignment = alignment[Math.floor(Math.random() * 3)];
+
+const output = `You will be born as a ${randomRace} with the vocation of ${randomClass}. Your heart and mind are filled with ${randomAlignment}`
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('buildmaker')
+        .setDescription('Randomized build for a new BG3 playthrough!'),
+    async execute(interaction) {
+        await interaction.reply(output)
+    }
+}
+
+
+
